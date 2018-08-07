46311905e4aa1a4869ad916e673a9b23b68bb132
Shelby Trinca <shelby.trinca@gmail.com>
2018-08-07 22:34:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,11 +63,18 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+<<<<<<< HEAD
   var shoppingCartTotal = total();
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     cart = [];
     return `Your total cost is $${shoppingCartTotal}, which will be charged to the card ${cardNumber}.`;
+=======
+  if (cardNumber === 'undefined') {
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    
+>>>>>>> 555007714b0bff3d159d212bbd6a84455fff4040
   }
 }
